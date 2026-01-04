cc0ca6884a13199b2fe5344ebf02c29f3919f8fa
Gaurav Verma <briskgaurav@gmail.com>
2026-01-04 23:41:38 +0530

    BIKE SCENE

--- src/components/BikeExperience/Experience.jsx
+++ src/components/BikeExperience/Experience.jsx
@@ -1,38 +1,50 @@
 "use client";
 import {
   Center,
+  ContactShadows,
   Environment,
   MeshReflectorMaterial,
   OrbitControls,
+  Stage,
+  Stats,
 } from "@react-three/drei";
 import React from "react";
 import { Model } from "./Model";
 import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Experience() {
   return (
-    <div className="fixed inset-0 w-screen h-screen bg-white">
+    <div className="fixed inset-0 w-screen h-screen bg-black">
       <Canvas>
-        <Environment preset="warehouse" environmentIntensity={1} />
+        <fog attach="fog" args={["#ecedef", 5, 10]} />
+        <Stats />
+        {/* <Environment preset="warehouse"  environmentIntensity={1} /> */}
+        <Stage adjustCamera={false} />
         <Center>
           <Model />
-          <mesh rotation-x={-Math.PI / 2}> 
-            <planeGeometry args={[15, 10]} />
+          <mesh rotation-x={-Math.PI / 2}>
+            <planeGeometry args={[100, 10]} />
             <MeshReflectorMaterial
-              color="#ffffff"
-              mirror={0.8}
-              metalness={0.1}
-              roughness={0.1}
+              side={THREE.DoubleSide}
               blur={[300, 100]}
-              mixBlur={.2}
-              mixStrength={1}
-              mixContrast={1}
-              resolution={1024}
-              distortion={0.1}
-              distortionMap={null}
+              mirror={.4}
+              resolution={2048}
+              mixBlur={.7}
+              // mixStrength={80}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.4}
+              maxDepthThreshold={1.4}
+              color="#ffffff"
+              metalness={0.5}
             />
           </mesh>
         </Center>
+        <mesh position-y={-1.5} position-z={-15} scale={100}>
+          <planeGeometry args={[10, 10]} />
+          <meshBasicMaterial color="black" />
+        </mesh>
         <OrbitControls />
       </Canvas>
     </div>
