446f68692ce0c2a453332daefde43b8a2b9505ad
Pritam <pritamdas5370@gmail.com>
2026-01-06 00:36:22 +0530

    push

--- src/components/BikeExperience/Experience.jsx
+++ src/components/BikeExperience/Experience.jsx
@@ -13,6 +13,7 @@ import { Model } from "./Model";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 
+
 export default function Experience() {
   return (
     <div className="fixed inset-0 w-screen h-screen bg-[#ecedef]">
@@ -23,33 +24,13 @@ export default function Experience() {
         <Stage adjustCamera={false} />
         <Center>
           <Model />
-          <mesh rotation-x={-Math.PI / 2}>
-            <planeGeometry args={[100, 40]} />
-            <MeshReflectorMaterial
-              side={THREE.DoubleSide}
-              blur={[300, 100]}
-              mirror={0.4}
-              resolution={2048}
-              mixBlur={0.7}
-              // mixStrength={80}
-              roughness={1}
-              distortionTexture={
-                "https://as1.ftcdn.net/jpg/04/63/98/24/1000_F_463982482_I310LXkFzX7Bf0RB7ojgVXmmenExRSOL.jpg"
-              }
-              distortion={1.90}
-              depthScale={1.2}
-              minDepthThreshold={0.4}
-              maxDepthThreshold={1.4}
-              color="#ffffff"
-              metalness={0.5}
-            />
-          </mesh>
+          
         </Center>
         <mesh position-y={-1.5} position-z={-15} scale={100}>
           <planeGeometry args={[10, 10]} />
           <meshBasicMaterial color="black" />
         </mesh>
-        {/* <OrbitControls /> */}
+        <OrbitControls />
       
       </Canvas>
     </div>
--- src/components/BikeExperience/Model.jsx
+++ src/components/BikeExperience/Model.jsx
@@ -1,34 +1,67 @@
-
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
-import { degToRad } from 'three/src/math/MathUtils'
+import React, { useRef, useMemo } from "react";
+import { MeshReflectorMaterial, useGLTF, useTexture } from "@react-three/drei";
+import { degToRad } from "three/src/math/MathUtils";
+import * as THREE from "three";
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/model/BikeFixed.glb')
+  const { nodes, materials } = useGLTF("/model/BikeFixed.glb");
+  const texture = useTexture(
+    "https://as1.ftcdn.net/jpg/04/63/98/24/1000_F_463982482_I310LXkFzX7Bf0RB7ojgVXmmenExRSOL.jpg"
+  );
+
+
   return (
-    <group rotation={[degToRad(0), degToRad(-90), degToRad(0)]} {...props} scale={0.04} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Body_Bike_Body_0.geometry}
-          material={materials['Bike_Body.001']}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Frame_Bike_Other_0.geometry}
-          material={materials['Bike_Other.001']}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes['Wheels_Wheels&Cables_0'].geometry}
-          material={materials['WheelsCables.001']}
-        />
+    <>
+      <group
+        rotation={[degToRad(0), degToRad(-90), degToRad(0)]}
+        {...props}
+        scale={0.04}
+        dispose={null}
+      >
+        <group rotation={[-Math.PI / 2, 0, 0]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Body_Bike_Body_0.geometry}
+            material={materials["Bike_Body.001"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Frame_Bike_Other_0.geometry}
+            material={materials["Bike_Other.001"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes["Wheels_Wheels&Cables_0"].geometry}
+            material={materials["WheelsCables.001"]}
+          />
+        </group>
       </group>
-    </group>
-  )
+
+      <mesh rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[28, 28]}  />
+        <MeshReflectorMaterial
+          side={THREE.DoubleSide}
+          blur={[300, 100]}
+          mirror={0.5}
+          resolution={512}
+          mixBlur={0.7}
+          // mixStrength={40}
+          roughness={1}
+          distortionMap={texture}
+          distortion={.24}
+          depthScale={10.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          // reflectorOffset={0.1}
+          color="#ffffff"
+          metalness={1}
+        />
+      </mesh>
+    </>
+  );
 }
 
-useGLTF.preload('/BikeFixed.glb')
+useGLTF.preload("/BikeFixed.glb");
